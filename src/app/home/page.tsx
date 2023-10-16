a5b4dfcd07766698582cec93057d591a095d076c
Andre Nascimento <andredossantosnascimento1510@gmail.com>
2023-10-16 04:29:52 -0300

    feat: Default values

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -111,6 +111,7 @@ export default function Home() {
                       autoComplete="codigo-produto"
                       className="flex w-1/4 me-2 h-10 rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6"
                       placeholder="Código do produto"
+                      defaultValue={codigo}
                       {...register("codigo")}
                     />
                     <input
@@ -120,6 +121,7 @@ export default function Home() {
                       required
                       className="flex w-2/12 me-2 h-10 rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6"
                       placeholder="Offset"
+                      defaultValue={offset}
                       {...register("offset")}
                     />
                     <input
@@ -129,6 +131,7 @@ export default function Home() {
                       required
                       className="flex w-2/12 me-2 h-10 rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6"
                       placeholder="Limite"
+                      defaultValue={limit}
                       {...register("limit")}
                     />
                     <button
